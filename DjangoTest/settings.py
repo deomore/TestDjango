a2fad498b6bf60cca9a91f9ef3bbb92d9ef7cdd4
deomore <iv@webizi.ru>
2025-05-21 15:14:11 +0300

    upd CORS

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -59,11 +59,13 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,6 +74,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'DjangoTest.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 TEMPLATES = [
     {
